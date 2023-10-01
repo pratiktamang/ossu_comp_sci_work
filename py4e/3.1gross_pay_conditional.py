46cb294ec02d61d58419b4875c5ce1b94a70e081
pratiktamang <pratiktamang13@gmail.com>
2023-10-01 15:40:50 +0100

    update float

--- py4e/3.1gross_pay_conditional.py
+++ py4e/3.1gross_pay_conditional.py
@@ -1,16 +1,21 @@
-# Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
-# Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked
-# above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
-# You should use input to read a string and float() to convert the string to a number.
-# Do not worry about error checking the user input - assume the user types numbers properly.
+# Write a program to prompt the user for hours and rate
+# per hour using input to compute gross pay.
+# Pay the hourly rate for the hours up to 40 and 1.5
+# times the hourly rate for all hours worked
+# above 40 hours. Use 45 hours and a rate of 10.50 per
+# hour to test the program (the pay should be 498.75).
+# You should use input to read a string and float()
+# to convert the string to a number.
+# Do not worry about error checking the user input -
+# assume the user types numbers properly.
 
 hrs = input("Enter Hours:")
 h = float(hrs)
-rate = input("Enter Rate:")
+rate = float(input("Enter Rate:"))
 
 if h <= 40:
-    pay = h * float(rate)
+    pay = h * rate
 else:
-    pay = 40 * float(rate) + (h - 40) * float(rate) * 1.5
+    pay = 40 * rate + (h - 40) * rate * 1.5
 
 print(pay)
