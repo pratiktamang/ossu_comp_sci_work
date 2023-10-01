81acd549923ebbe25e65efcd951e97b902f02de6
pratiktamang <pratiktamang13@gmail.com>
2023-10-01 15:44:55 +0100

    add error check

--- py4e/score_grade.py
+++ py4e/score_grade.py
@@ -13,15 +13,16 @@
 
 score = float(input("Enter Score: "))
 
-if score > 1.0 or score < 0:
-    print("too smart")
-elif score >= 0.9:
-    print("A")
-elif score >= 0.8:
-    print("B")
-elif score >= 0.7:
-    print("C")
-elif score >= 0.6:
-    print("D")
+if 0.0 <= score <= 1.0:
+    if score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    else:
+        print("F")
 else:
-    print("F")
+    print("error")
