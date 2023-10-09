a5cc7528d94a05994d2fa9feed59b890300f96d7
pratiktamang <pratiktamang13@gmail.com>
2023-10-09 18:30:35 +0100

    rpc with validation for incorrect user input

--- python/practice_python/rock_paper_scissors.py
+++ python/practice_python/rock_paper_scissors.py
@@ -13,29 +13,33 @@
 import random
 
 
-CHOICES = {1: "rock", 2: "paper", 3: "scissors"}
+CHOICES = ["rock", "paper", "scissors"]
 WINNING_COMBINATIONS = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
 
 
 def game():
     user_choice = user_input()
     comp_choice = computer_input()
-
     check_winner(user_choice, comp_choice)
     play_next_round()
 
 
 def user_input():
-    user_input = input("Choose your weapon: ")
-    return user_input
+    while True:
+        user_input = input("Choose your weapon: ")
+        if user_input in CHOICES:
+            return user_input
+        else:
+            print("Not a valid options try again")
 
 
 def computer_input():
-    computer_input = CHOICES[random.randint(1, 3)]
-    return computer_input
+    return random.choice(CHOICES)
 
 
 def check_winner(user_choice, computer_choice):
+    print(f"You chose {user_choice}")
+    print(f"Computer chose {computer_choice}")
     if user_choice == computer_choice:
         print("It's a tie!")
     elif WINNING_COMBINATIONS[user_choice] == computer_choice:
@@ -53,3 +57,29 @@ def play_next_round():
 
 
 game()
+
+# while True:
+#     game_dict = {"rock": 1, "scissors": 2, "paper": 3}
+#     player_a = str(input("Player a: "))
+#     player_b = str(input("Player b: "))
+#     a = game_dict.get(player_a)
+#     b = game_dict.get(player_b)
+#     dif = a - b
+
+#     if dif in [-1, 2]:
+#         print("player a wins.")
+#         if str(input("Do you want to play another game, yes or no?\n")) == "yes":
+#             continue
+#         else:
+#             print("game over.")
+#             break
+#     elif dif in [-2, 1]:
+#         print("player b wins.")
+#         if str(input("Do you want to play another game, yes or no?\n")) == "yes":
+#             continue
+#         else:
+#             print("game over.")
+#             break
+#     else:
+#         print("Draw.Please continue.")
+#         print("")
