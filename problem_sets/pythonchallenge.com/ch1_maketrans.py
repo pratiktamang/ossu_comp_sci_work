2cbd82d5f621097cd28b24faf803b6fb3f570462
pratiktamang <pratiktamang13@gmail.com>
2024-01-30 01:33:24 +0000

    ruby answer

--- problem_sets/pythonchallenge.com/ch1_maketrans.py
+++ problem_sets/pythonchallenge.com/ch1_maketrans.py
@@ -1,3 +1,5 @@
+# http://www.pythonchallenge.com/pc/def/map.html
+
 """
 str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 k => m
@@ -71,3 +73,12 @@ def translate(str):
 
 
 print(translate(str))
+
+"""
+Ruby:
+    def translate(str)
+        input_str = "abcdefghijklmnopqrstuvwxyz"
+        output_str = "cdefghijklmnopqrstuvwxyzab"
+        str.tr(input_str, output_str)
+    end
+"""
