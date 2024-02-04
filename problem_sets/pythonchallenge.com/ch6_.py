298672f4e559c437225be0709eb24dd5e7ad2f23
pratiktamang <pratiktamang13@gmail.com>
2024-02-04 04:03:39 +0000

    puzzle 7

--- problem_sets/pythonchallenge.com/ch6_.py
+++ problem_sets/pythonchallenge.com/ch6_.py
@@ -1,24 +1,46 @@
 # http://www.pythonchallenge.com/pc/def/channel.html
-import zipfile
+# import zipfile
 
 
-def hint():
-    comments = []
-    num = "90052"
-    zipped_file = zipfile.ZipFile("./ch6_zip_file.zip")
+# def hint():
+#     comments = []
+#     num = "90052"
+#     zipped_file = zipfile.ZipFile("./ch6_zip_file.zip")
 
-    while True:
-        file = num + ".txt"
-        content = zipped_file.read(file).decode("utf-8")
-        comments.append(zipped_file.getinfo(file).comment.decode("utf-8"))
-        if content.startswith("Next nothing is "):
-            num = content.split(" ")[-1]
-        else:
-            break
-    print("".join(comments))
+#     while True:
+#         file = num + ".txt"
+#         content = zipped_file.read(file).decode("utf-8")
+#         comments.append(zipped_file.getinfo(file).comment.decode("utf-8"))
+#         if content.startswith("Next nothing is "):
+#             num = content.split(" ")[-1]
+#         else:
+#             break
+#     print("".join(comments))
 
 
-hint()
+# hint()
+
+# with regular expression
+import zipfile, re
+
+f = zipfile.ZipFile("./ch6_zip_file.zip")
+print(f.read("readme.txt").decode("utf-8"))
+
+num = "90052"
+
+comments = []
+
+while True:
+    content = f.read(num + ".txt").decode("utf-8")
+    comments.append(f.getinfo(num + ".txt").comment.decode("utf-8"))
+    print(content)
+    match = re.search("Next nothing is (\d+)", content)
+    if match == None:
+        break
+    num = match.group(1)
+
+print("".join(comments))
+
 
 """
 ****************************************************************
